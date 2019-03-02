158172bebca3883ff422dfd1a3b89a93504b4e9c
LadyEbony <jgarc129@ucr.edu>
2019-03-02 14:14:39 -0800

    Added timer.

---
+++ Unity/Paleontology/Assets/Scripts/DirtController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtController : MonoBehaviour
+{
+    public GameObject DirtPrefab;
+
+    public float x, y, z;
+    public float length, width, height;
+
+    [ContextMenu("Spawn")]
+    private void SpawnDirts(){
+        var children = new List<GameObject>();
+        foreach (Transform child in transform)
+            children.Add(child.gameObject);
+        children.ForEach(child => DestroyImmediate(child));
+
+        var basePos = transform.position;
+        var xStart = -(x - 1) * (length / 2);
+        var zStart = -(z - 1) * (width / 2);
+        for (var i = 0; i < x; i++){
+            for(var j = 0; j < y; j++){
+                for (var k = 0; k < z; k++) {
+                    Instantiate(DirtPrefab, basePos + new Vector3(xStart + length * i, height * j, zStart + width * k), Quaternion.identity, transform);
+                }
+            }
+        }
+    }
+
+}
---
+++ Unity/Paleontology/Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float TimeLimit = 360f;
+    public float CurrentTime;
+
+    public TextMeshProUGUI TextGUI;
+
+    private void Update() {
+        CurrentTime = TimeLimit - Time.timeSinceLevelLoad;
+        if (CurrentTime <= 0.0f){
+            // STOP GAME
+            // Reset level?
+        } else {
+            TextGUI.text = string.Format("{0,0:D2}:{1,0:D2}", Mathf.FloorToInt(CurrentTime / 60f), Mathf.FloorToInt(CurrentTime % 60.0f));
+        }
+    }
+}
