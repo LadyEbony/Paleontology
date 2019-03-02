afb3e05dc686a4684952b3644e368c3cdddb70d4
LadyEbony <jgarc129@ucr.edu>
2019-03-02 12:19:34 -0800

    Added bone breaking.

---
+++ Unity/Paleontology/Assets/Scripts/FossilController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FossilController : MonoBehaviour
+{
+
+    public void CreateFossilPiece(GameObject gameObj){
+        gameObj.transform.SetParent(null, true);
+        gameObj.AddComponent<Rigidbody>();
+    }
+
+    ContactPoint[] contacts = new ContactPoint[128];
+    int contactLength;
+
+    public void OnCollisionEnter(Collision collision) {
+        contactLength = collision.GetContacts(contacts);
+
+        // Get all unique colliders
+        List<Collider> uniqueColliders = new List<Collider>();
+        Collider col;
+        for (var i = 0; i < contactLength; i++) {
+            col = contacts[i].thisCollider;
+            if (!uniqueColliders.Contains(col)) {
+                uniqueColliders.Add(col);
+            }
+        }
+        Debug.LogFormat("Count: {0}", uniqueColliders.Count);
+
+        // Apply integrity updates
+        var force = (collision.impulse / Time.fixedDeltaTime).magnitude;    // Get total force
+        foreach(var uCol in uniqueColliders){
+            uCol.GetComponent<FossilPiece>().ApplyIntegrityUpdate(-force);
+        }
+    }
+
+}
---
+++ Unity/Paleontology/Assets/Scripts/FossilPiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FossilPiece : MonoBehaviour
+{
+    public float integrity;
+
+    public void ApplyIntegrityUpdate(float delta){
+        integrity += delta;
+
+        if (integrity <= 0)
+            CreateFossilPiece();
+    }
+
+    private void CreateFossilPiece() {
+        var gameObj = gameObject;
+        gameObj.transform.SetParent(null, true);
+        gameObj.AddComponent<Rigidbody>();
+    }
+
+}
