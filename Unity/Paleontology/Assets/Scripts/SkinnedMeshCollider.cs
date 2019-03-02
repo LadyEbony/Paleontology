0fd9c7a5a8b101d819badc717b826db3eb433541
theclone <mrperson7895@gmail.com>
2019-03-02 14:41:49 -0800

    add VR sdk and grab testing

---
+++ Unity/Paleontology/Assets/Scripts/SkinnedMeshCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+public class SkinnedMeshCollider : MonoBehaviour
+{
+    private MeshCollider meshCollider;
+    private SkinnedMeshRenderer meshRenderer;
+    void Start()
+    {
+        meshCollider = GetComponent<MeshCollider>();
+        meshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
